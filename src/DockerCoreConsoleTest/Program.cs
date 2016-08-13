2595d16b2496cd4b98836dcc1bdcc0b6d0823730
Sebastian Krysmanski <noreply@manski.net>
2016-08-13 15:17:41 +0200

    Added NuGet dependency

--- src/DockerCoreConsoleTest/Program.cs
+++ src/DockerCoreConsoleTest/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace DockerCoreConsoleTest
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DockerCoreConsoleTest
 {
     public class Program
     {
         public static void Main()
         {
+            Console.WriteLine(
+                $"Hello, Docker and {typeof(JsonConvert).FullName}!"
+            );
         }
     }
 }
